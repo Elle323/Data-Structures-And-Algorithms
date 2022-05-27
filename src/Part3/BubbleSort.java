8bc46c97e1bf43301a84c0f1ba952c68f4847eb2
Natallia Zlo <zlotnikava@gmail.com>
2022-05-27 12:29:52 -0500

    Add Natasha's branch Add Bubble Sort from Mosh

--- src/Part3/BubbleSort.java
+++ src/Part3/BubbleSort.java
@@ -25,4 +25,27 @@ public class BubbleSort {
         return arr;
     }
 
+    public static int [] bubbleSortMosh(int [] arr){
+        boolean isSorted;
+        for(int i = 0; i < arr.length; i++){
+            isSorted = true;
+            for(int j = 1; j < arr.length-i; j++){
+                if(arr[j]< arr[j-1]){
+                    swap(arr, j, j-1);
+                    isSorted = false;
+                }
+            }
+            if(isSorted){
+                return arr;
+            }
+        }
+        return arr;
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
 }
---
+++ src/Part3/Main.java
@@ -0,0 +1,14 @@
+package Part3;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] array = {-3, -5, 10, 9, 7, 0, -1, -1};
+        BubbleSort.bubbleSortMosh(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+
+}
